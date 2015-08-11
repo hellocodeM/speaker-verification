7a6e851f3304b24f1ac1be631f1ebbc6be82779a
Hello Code <HelloCodeMing@users.noreply.github.com>
2015-08-11 10:33:45 +0800

    Update mfcc-features.cc

--- lib/SV/mfcc-features.cc
+++ lib/SV/mfcc-features.cc
@@ -15,7 +15,6 @@
 int main(int argc, char *argv[])
 {
     using namespace Aquila;
-    const size_t input_size = 1024;
     const size_t frame_size = 1024;
     const size_t frame_overlap = 512;
 
@@ -23,9 +22,9 @@ int main(int argc, char *argv[])
         ming::println("You should add an .wav");
         return 1;
     }
-    Aquila::WaveFile wave(argv[1]);
-    Aquila::Mfcc mfcc(input_size);
-    std::vector<std::vector<Aquila::SampleType>> features;
+    WaveFile wave(argv[1]);
+    Mfcc mfcc(frame_size);
+    std::vector<std::vector<SampleType>> features;
     
     // wave property
     //ming::printf("wave name: %\n", wave.getFilename());
@@ -36,9 +35,12 @@ int main(int argc, char *argv[])
 
     // mfcc
     FramesCollection frames(wave, frame_size, frame_overlap);
+    HannWindow hann(frame_size);
     features.reserve(frames.count());
     for (auto frame : frames) {
-        features.emplace_back(mfcc.calculate(frame));
+        SignalSource sig(frame.toArray(), frame.length());
+        sig *= hann;
+        features.emplace_back(mfcc.calculate(sig.toArray()));
     }
 
     for (auto&& i: features)
