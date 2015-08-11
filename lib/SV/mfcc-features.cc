18487d465928e468fbd8a6c4a83ed9e28787254e
Hello Code <HelloCodeMing@users.noreply.github.com>
2015-08-11 11:24:31 +0800

    refactor

--- lib/SV/mfcc-features.cc
+++ lib/SV/mfcc-features.cc
@@ -9,14 +9,13 @@
 #include <iostream>
 #include <iterator>
 
-#include <ming/all.hpp>
 #include <aquila/aquila.h>
 
 int main(int argc, char *argv[])
 {
     using namespace Aquila;
-    const size_t frame_size = 1024;
-    const size_t frame_overlap = 512;
+    constexpr size_t frame_size = 1024;
+    constexpr size_t frame_overlap = 512;
 
     if (argc < 2) {
         ming::println("You should add an .wav");
@@ -24,26 +23,22 @@ int main(int argc, char *argv[])
     }
     WaveFile wave(argv[1]);
     Mfcc mfcc(frame_size);
+    HannWindow hann(frame_size);
+    FramesCollection frames(wave, frame_size, frame_overlap);
     std::vector<std::vector<SampleType>> features;
-    
-    // wave property
-    //ming::printf("wave name: %\n", wave.getFilename());
-    //ming::printf("samples count: %\n", wave.getSamplesCount());
-    //ming::printf("sample frequency: %\n", wave.getSampleFrequency());
-    //ming::printf("audio length: %\n", wave.getAudioLength());
-    //ming::printf("bits per sample: %\n", wave.getBitsPerSample());
 
     // mfcc
-    FramesCollection frames(wave, frame_size, frame_overlap);
-    HannWindow hann(frame_size);
     features.reserve(frames.count());
     for (auto frame : frames) {
-        SignalSource sig(frame.toArray(), frame.length());
+        SignalSource sig(frame.toArray(), frame.length(), frame.getSampleFrequency());
         sig *= hann;
-        features.emplace_back(mfcc.calculate(sig.toArray()));
+        features.emplace_back(mfcc.calculate(sig));
     }
 
-    for (auto&& i: features)
-        ming::println(i);
+    for (auto&& i : res) {
+		for (auto j : i)
+			std::cout << j << " ";
+		std::cout << "\n";
+	}
     return 0;
 }
